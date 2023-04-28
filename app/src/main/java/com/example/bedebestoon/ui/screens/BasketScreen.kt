9d7624be5416be4278fbc97bee975fef6eea8463
AliAfroozi <ali.afroozi.dev@gmail.com>
2023-04-28 16:22:24 +0430

    FIX: put shopping cart image in center of page when basket list is empty.
    Discription: also add complete purchase bottun.

--- app/src/main/java/com/example/bedebestoon/ui/screens/BasketScreen.kt
+++ app/src/main/java/com/example/bedebestoon/ui/screens/BasketScreen.kt
@@ -1,11 +1,10 @@
 package com.example.bedebestoon.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ShoppingCart
@@ -16,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,6 +23,7 @@ import androidx.navigation.NavHostController
 import com.example.bedebestoon.MainActivity
 import com.example.bedebestoon.db.viewModels.BasketEntityViewModel
 import com.example.bedebestoon.ui.components.basket.BasketListItem
+import com.example.bedebestoon.ui.theme.Dark
 
 @Composable
 fun BasketScreen(
@@ -35,45 +36,94 @@ fun BasketScreen(
 
 
 
-    LazyColumn() {
-        item {
-            Row(modifier = Modifier.padding(horizontal = 8.dp, vertical = 16.dp)) {
-                IconButton(onClick = { navController.popBackStack() }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
-                }
-                Spacer(modifier = Modifier.width(8.dp))
-                Column() {
-                    Text(text = "Shopping Cart", style = MaterialTheme.typography.h2)
-                    Spacer(modifier = Modifier.height(8.dp))
-                    if (basketList.value.isNotEmpty()) {
-                        Text(
-                            text = if (basketList.value.size == 1) "${basketList.value.size} item" else "${basketList.value.size} items" ,
-                            style = MaterialTheme.typography.body1,
-                            fontSize = 14.sp,
-                            fontWeight = FontWeight.Light,
-                            color = Color.Gray
-                        )
+    Box() {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            item {
+                Row(
+                    modifier = Modifier.padding(horizontal = 4.dp, vertical = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
                     }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Column() {
+                        Text(text = "Shopping Cart", style = MaterialTheme.typography.h2)
+                        Spacer(modifier = Modifier.height(8.dp))
+                        if (basketList.value.isNotEmpty()) {
+                            Text(
+                                text = if (basketList.value.size == 1) "${basketList.value.size} item" else "${basketList.value.size} items",
+                                style = MaterialTheme.typography.body1,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Light,
+                                color = Color.Gray
+                            )
+                        }
+                    }
+                }
+            }
+
+
+            if (basketList.value.isEmpty()) {
+                item {
+                    val screenHeight = LocalConfiguration.current.screenHeightDp / 3
+                    Spacer(modifier = Modifier.height(screenHeight.dp))
+                }
+                item {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(
+                                imageVector = Icons.Filled.ShoppingCart,
+                                contentDescription = "",
+                                modifier = Modifier.size(70.dp)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(text = "Your shopping cart is empty.", fontSize = 16.sp)
+                        }
+                    }
+                }
+            } else {
+                items(count = basketList.value.size) { index ->
+                    BasketListItem(
+                        basketList.value[index],
+                        basketEntityViewModel,
+                        navController
+                    )
+
+                    if (index == basketList.value.size -1 ){
+                        Spacer(modifier = Modifier.height(130.dp))
+                    }
+                }
+
+                item {
+
                 }
             }
         }
 
+        if (basketList.value.isNotEmpty()) {
+            Box(contentAlignment = Alignment.BottomCenter , modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Transparent)) {
 
-        if (basketList.value.isEmpty()){
-            item {
-                Box(modifier = Modifier.fillMaxSize() , contentAlignment = Alignment.Center) {
-                    Column() {
-                        Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = "" , modifier = Modifier.size(70.dp))
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(text = "Your shopping cart is empty." , fontSize = 16.sp)
-                    }
+                Button(onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Dark),
+                    shape = RoundedCornerShape(15.dp),
+                    modifier = Modifier
+                        .padding(25.dp)
+                        .fillMaxWidth()
+                        .height(50.dp)) {
+                    Text(
+                        text = "Complete your purchase",
+                        color = Color.White,
+                        style = MaterialTheme.typography.button,
+                        fontSize = 16.sp
+                    )
                 }
-            }
-        }else{
-            items(count = basketList.value.size) { index ->
-                BasketListItem(basketList.value[index])
+
             }
         }
     }
+
 
 }
